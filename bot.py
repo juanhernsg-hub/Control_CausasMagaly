359f7bbf2867a734106e023878af70521e511066
juanhernsg-hub <juanhernsg@gmail.com>
2026-05-23 11:24:05 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 # 🔑 CONFIGURACIÓN PRINCIPAL
 TOKEN_TELEGRAM = "8867621977:AAE_fjmpD9aTnpgjfC5RHNEDSRhUsCwG6WwI"  
-WEBAPP_URL = "8867621977:AAE_fjmpD9aTnpgjfC5RHNEDSRhUsCwG6Ww"
+WEBAPP_URL = "https://docs.google.com/spreadsheets/d/1-CGHw4jFXPraBcNMPOL9n4EHnCu3jl0U2aib3lfMkhU/edit?usp=sharing"
 
 bot = telebot.TeleBot(TOKEN_TELEGRAM)
 
